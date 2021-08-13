392c8337ca5d2d25e67bd9caa3687ae50a7612cc
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-08-12 21:03:23 -0300

    Separando actions e reducers

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,14 +3,14 @@ import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+import * as exempleActions from '../../store/modules/example/actions';
 
 export default function Login() {
   const dispatch = useDispatch();
+
   function handleClick(e) {
     e.preventDefault();
-    dispatch({
-      type: 'BOTAO_CLICADO',
-    });
+    dispatch(exempleActions.clicaBotao());
   }
   return (
     <>
